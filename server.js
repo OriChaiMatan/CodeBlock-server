21ad60236299617914fafe85ea468844f835c513
OriChaiMatan <ori.chaimatan@gmail.com>
2024-07-24 19:35:24 +0300

    Added socket service

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import cookieParser from 'cookie-parser'
+import { createServer } from "node:http";
 
 import { loggerService } from './services/logger.service.js'
+import { socketService } from './services/socket.service.js'
 
 const app = express()
+const server = createServer(app)
 
 const corsOptions = {
 	origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://localhost:3000', 'https://codeblocksserverorichaimatan.onrender.com'],
@@ -19,6 +22,8 @@ app.use(express.json())
 
 import { codeblockRoutes } from './api/codeblocks/codeblock.routes.js'
 app.use('/api/codeblock', codeblockRoutes)
+
+socketService.setupSocketAPI(server)
 
 app.get('/api/logs', async (req, res) => {
 	res.sendFile(process.cwd() + '/logs/backend.log')
---
+++ services/socket.service.js
@@ -0,0 +1,86 @@
+import { Server } from 'socket.io'
+import { loggerService } from './logger.service.js'
+
+var gIo = null
+var mentors = {}
+
+export function setupSocketAPI(http) {
+    gIo = new Server(http, { cors: { origin: '*' } })
+
+    gIo.on('connection', socket => {
+        loggerService.info(`New connected socket [id: ${socket.id}]`)
+
+        socket.on('disconnect', () => {
+            loggerService.info(`Socket disconnected [id: ${socket.id}]`)
+            for (const codeblockId in mentors) {
+                if (mentors[codeblockId] === socket.id) {
+                    delete mentors[codeblockId]
+                    broadcast({
+                        type: 'redirect-to-lobby',
+                        data: { message: 'Mentor has left the code block' },
+                        room: codeblockId
+                    })
+                }
+            }
+        })
+
+        socket.on('join-codeblock', codeblockId => {
+            socket.join(codeblockId)
+            let role = 'Student'
+            if (!mentors[codeblockId]) {
+                mentors[codeblockId] = socket.id
+                role = 'Mentor'
+                loggerService.info(`Mentor joined codeblock [id: ${codeblockId}] [socket: ${socket.id}]`)
+            } else {
+                loggerService.info(`Student joined codeblock [id: ${codeblockId}] [socket: ${socket.id}]`)
+            }
+            socket.emit('set-role', role)
+            updateStudentCount(codeblockId)
+        })
+
+        socket.on('leave-codeblock', codeblockId => {
+            socket.leave(codeblockId)
+            if (mentors[codeblockId] === socket.id) {
+                delete mentors[codeblockId]
+                broadcast({
+                    type: 'redirect-to-lobby',
+                    data: { message: 'Mentor has left the code block' },
+                    room: codeblockId
+                })
+            }
+            updateStudentCount(codeblockId)
+        })
+
+        socket.on('code-change', ({ codeblockId, code }) => {
+            socket.to(codeblockId).emit('code-update', code)
+        })
+
+        socket.on('get-students-count', codeblockId => {
+            updateStudentCount(codeblockId)
+        })
+    })
+}
+
+function updateStudentCount(codeblockId) {
+    const studentCount = gIo.sockets.adapter.rooms.get(codeblockId)?.size - 1 || 0;
+    gIo.to(codeblockId).emit('students-count', studentCount);
+}
+
+async function broadcast({ type, data, room = null }) {
+    loggerService.info(`Broadcasting event: ${type}`)
+
+    if (room) {
+        loggerService.info(`Emit to room: ${room}`)
+        gIo.to(room).emit(type, data)
+    } else {
+        loggerService.info(`Emit to all`)
+        gIo.emit(type, data)
+    }
+}
+
+export const socketService = {
+    // set up the sockets service and define the API
+    setupSocketAPI,
+    // Send to all sockets (otherwise broadcast to a room / to all)
+    broadcast,
+}
